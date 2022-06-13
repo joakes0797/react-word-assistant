3623f5afbaea0cd4bbac8a389abc5167040e8aa1
Jill Oakes <jericho_rules@hotmail.com>
2022-06-13 19:35:40 -0400

    Complete yellowLetter functionality

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -12,6 +12,11 @@ export function SearchForm() {
             B03: '',
             B04: '',
             B05: '',
+            B06: '',
+            B07: '',
+            B08: '',
+            B09: '',
+            B10: ''
         },
         onSubmit: values => {
             var greenLetters = '';
@@ -23,16 +28,43 @@ export function SearchForm() {
             (values.B05 === '') ? greenLetters += '.' : greenLetters += values.B05.toLowerCase();
             
             result = greenLetters;
-            // console.log(`The result is ${result}`);
             var regex = new RegExp(`${result}`);
-            // console.log(`The result is ${regex}`);
-            console.log(`this is a new search-------------`)
-            // var bar = answers.filter(word => word.match(/c..../g));
-            var bar = answers.filter(word => word.match(regex));
-
-            for (let i = 0; i < bar.length; i++){
-                console.log(bar[i]);
+            console.log(`this is a new green search-------------`)
+            var foo = answers.filter(word => word.match(regex));
+            for (let i = 0; i < foo.length; i++){
+                console.log(foo[i]);
             }
+
+            var y1 = '';
+            var y2 = '';
+            var y3 = '';
+            var y4 = '';
+            var y5 = '';
+
+            (values.B06 === '') ? y1 = '.' : y1 = values.B06.toLowerCase();
+            (values.B07 === '') ? y2 = '.' : y2 = values.B07.toLowerCase();
+            (values.B08 === '') ? y3 = '.' : y3 = values.B08.toLowerCase();
+            (values.B09 === '') ? y4 = '.' : y4 = values.B09.toLowerCase();
+            (values.B10 === '') ? y5 = '.' : y5 = values.B10.toLowerCase();
+
+            console.log(`this is the yellow search on greens -------------`)
+            var bar = foo.filter(word => word.match(y1));
+            var bar2 = bar.filter(word => word.match(y2));
+            var bar3 = bar2.filter(word => word.match(y3));
+            var bar4 = bar3.filter(word => word.match(y4));
+            var bar5 = bar4.filter(word => word.match(y5));            
+            
+            for (let i = 0; i < bar5.length; i++){
+                console.log(bar5[i]);
+            }
+
+
+
+
+
+
+
+
         }
     });
     return (
@@ -48,15 +80,15 @@ export function SearchForm() {
                     <input onChange={formik.handleChange} value={formik.values.B04} type="text" pattern="[a-zA-Z]" title="No spaces, numbers, or special characters." id="B04" name="B04" maxLength="1" />
                     <input onChange={formik.handleChange} value={formik.values.B05} type="text" pattern="[a-zA-Z]" title="No spaces, numbers, or special characters." id="B05" name="B05" maxLength="1" />
                 </div>
-                {/* <div className="yellow-letters">
+                <div className="yellow-letters">
                     <h3>Enter any yellow letters.</h3>
-                    <input className="form-field" placeholder="" name="B06" maxlength="1" />
-                    <input className="form-field" placeholder="" name="B07" maxlength="1" />
-                    <input className="form-field" placeholder="" name="B08" maxlength="1" />
-                    <input className="form-field" placeholder="" name="B09" maxlength="1" />
-                    <input className="form-field" placeholder="" name="B10" maxlength="1" />
+                    <input onChange={formik.handleChange} value={formik.values.B06} type="text" pattern="[a-zA-Z]" title="No spaces, numbers, or special characters." id="B06" name="B06" maxLength="1" />
+                    <input onChange={formik.handleChange} value={formik.values.B07} type="text" pattern="[a-zA-Z]" title="No spaces, numbers, or special characters." id="B07" name="B07" maxLength="1" />
+                    <input onChange={formik.handleChange} value={formik.values.B08} type="text" pattern="[a-zA-Z]" title="No spaces, numbers, or special characters." id="B08" name="B08" maxLength="1" />
+                    <input onChange={formik.handleChange} value={formik.values.B09} type="text" pattern="[a-zA-Z]" title="No spaces, numbers, or special characters." id="B09" name="B09" maxLength="1" />
+                    <input onChange={formik.handleChange} value={formik.values.B10} type="text" pattern="[a-zA-Z]" title="No spaces, numbers, or special characters." id="B10" name="B10" maxLength="1" />
                 </div>
-                <div className="gray-letters">
+                {/* <div className="gray-letters">
                     <h3>Enter up to 10 gray letters.</h3>
                     <input className="form-field" placeholder="" name="B11" maxlength="1" />
                     <input className="form-field" placeholder="" name="B12" maxlength="1" />
